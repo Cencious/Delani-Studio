aff143300b6eb8a6dce28a78e7e82017fd6e1567
Santaclaus <santa@northpole.com>
2022-03-14 13:03:52 +0300

    indentation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-$(document).ready(function(){
-    $(".icon").click(function(){
-        $(this).find(".design-image").toggle()
-        $(this).find(".hidden-p").toggle() 
-    })
-    $(".column").hover(function(){
-        $(this).find(".overlay-text").toggle()
-    })
-    $("form").submit(function (e){
-        e.preventDefault()
-        let userName=$("#name").val()
-        alert(userName + " We have received your message. Thank you fro reaching out to us. ")
-    })
-})
+$(document).ready(function () {
+  $(".icon").click(function () {
+    $(this).find(".design-image").toggle();
+    $(this).find(".hidden-p").toggle();
+  });
+  $(".column").hover(function () {
+    $(this).find(".overlay-text").toggle();
+  });
+  $("form").submit(function (e) {
+    e.preventDefault();
+    let userName = $("#name").val();
+    alert(
+      userName +
+        " We have received your message. Thank you fro reaching out to us. "
+    );
+  });
+});
