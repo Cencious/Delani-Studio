19b726b16ab84af14d726322defcecb8d4696ddb
Santaclaus <santa@northpole.com>
2022-03-14 11:53:05 +0300

    add popup

--- js/script.js
+++ js/script.js
@@ -6,4 +6,9 @@ $(document).ready(function(){
     $(".column").hover(function(){
         $(this).find(".overlay-text").toggle()
     })
+    $("form").submit(function (e){
+        e.preventDefault()
+        let userName=$("#name").val()
+        alert(userName + " We have received your message. Thank you fro reaching out to us. ")
+    })
 })
